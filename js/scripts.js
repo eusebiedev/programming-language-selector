9107316db3aa265aed0edb4fcff2939f7684e9cf
Eusebie Siebenberg <siebenee@gmail.com>
2023-03-24 15:58:54 -0700

    Add branching OR logic to statements on scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 function languageSuggestion(event) {
   event.preventDefault();
   const name = document.getElementById("name").value;
+  const system = document.getElementById("system").value;
   const answer = document.querySelector("input[name='answer']:checked").value;
   const answer2 = document.querySelector("input[name='answer2']:checked").value;
   const flower = document.getElementById("flower").value;
@@ -23,6 +24,8 @@ function languageSuggestion(event) {
       result = "Swift";
     } else if (flower === "tulip") {
       result = "Python";
+    } else if (system = "macintosh" || "windows") {
+      result = "Ruby"
     }
   }
 
