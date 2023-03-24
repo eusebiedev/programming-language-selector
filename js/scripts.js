3b5d98f83b99f9651415f8b4e3f5556acaac9783
Eusebie Siebenberg <siebenee@gmail.com>
2023-03-24 13:28:18 -0700

    Add 1st  else/if statement to form function on scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,28 @@
 function languageSuggestion(event) {
   event.preventDefault();
   const name = document.getElementById("name").value;
-  const yesno = documument.querySelector("input[name='yesno']:checked").value;
+  const answer = document.querySelector("input[name='answer']:checked").value;
   const flower = document.getElementById("flower").value;
 
   let result;
-  if () {
-
-    
+  if (answer === "yes") {
+    if (flower === "rose") {
+      result = "Ruby";
+    } else if (flower === "tulip") {
+      result = "Python";
+    } 
   }
+  
+
+  
+
+  
 
 
 
 
-
-
-
-
-
-
-
-
-  document.querySelector("p#results").innerText = "Language Suggestion: " + result;
+  document.querySelector("p#results").innerText = "Hi: " + name + " " + "Your suggestion is: "
+  + result;
   document.querySelector("p#results").removeAttribute("class");
 }
 
