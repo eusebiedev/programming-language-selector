e34ddf2cb9b129f1b690cd5081ea886fbabcff1f
Eusebie Siebenberg <siebenee@gmail.com>
2023-03-24 14:10:51 -0700

    Add form reset to clear user input on scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 
 function languageSuggestion(event) {
   event.preventDefault();
+
   const name = document.getElementById("name").value;
   const answer = document.querySelector("input[name='answer']:checked").value;
   const flower = document.getElementById("flower").value;
@@ -11,17 +12,21 @@ function languageSuggestion(event) {
       result = "Ruby";
     } else if (flower === "tulip") {
       result = "Python";
-    } 
+    }
   }
-  
+    else if (answer === "no") {
+    if (flower === "rose") {
+      result = "Swift";
+    } else if (flower === "tulip") {
+        result = "Python";
+    }
+  }
 
   
 
   
 
-
-
-
+  document.getElementById("survey").reset();
   document.querySelector("p#results").innerText = "Hi: " + name + " " + "Your suggestion is: "
   + result;
   document.querySelector("p#results").removeAttribute("class");
